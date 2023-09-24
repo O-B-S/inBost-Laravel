a1a61ad780da83a62e1e4c1727d7ae994289bd6d
mrseck <ismaelseck11@gmail.com>
2023-09-24 05:53:44 +0000

    formulaire de contact back end en cours..

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Contact;
 
 class ContactController extends Controller
 {
-    //
+
+    public function create(Request $request) {
+        return view('/contact');
+      }
+    // Store Contact Form data
+    public function store(Request $request) {
+        // Form validation
+        $this->validate($request, [
+            'name' => 'required',
+            'forename' => 'required',
+            'email' => 'required|email',
+            'contact' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
+            'subject'=>'required',
+            'message' => 'required'
+         ]);
+        //  Store data in database
+        Contact::create($request->all());
+        // 
+        return back()->with('success', 'Message envoyé.');
+    }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -57,33 +57,75 @@
 
           </div>
 
-          <div class="col-lg-6">
-            <form action="{{ url('contact') }}" method="post" class="php-email-form">
+          <div class="col-lg-6 ">
+            <!-- Success message -->
+           @if(Session::has('success'))
+           <div class="alert alert-success">
+              {{Session::get('success')}}
+            </div>
+            @endif
+            <form action="{{ url('contact.store') }}" method="post" class="php-email-form">
                 {!! csrf_field() !!}
               <div class="row gy-4">
 
                 <div class="col-md-6">
-                  <input type="text" name="name" class="form-control" placeholder="Nom" required="">
+                  <input type="text" name="name" class="form-control {{ $errors->has('name') ? 'error' : '' }}" placeholder="Nom" required/>
+                  <!-- Error -->
+                   @if ($errors->has('name'))
+                     <div class="error">
+                         {{ $errors->first('name') }}
+                     </div>
+                   @endif
                 </div>
 
                 <div class="col-md-6">
-                  <input type="text" name="forename" class="form-control" placeholder="Prenoms" required="">
+                  <input type="text" name="forename" class="form-control {{ $errors->has('forename') ? 'error' : '' }}" placeholder="Prenoms" required/>
+                  <!-- Error -->
+                  @if ($errors->has('forename'))
+                  <div class="error">
+                      {{ $errors->first('forename') }}
+                  </div>
+                  @endif
                 </div>
 
                 <div class="col-md-6 ">
-                  <input type="email" class="form-control" name="email" placeholder="Email" required="">
+                  <input type="email" class="form-control {{ $errors->has('email') ? 'error' : '' }}" name="email" placeholder="Email" required/>
+                  <!-- Error -->
+                  @if ($errors->has('email'))
+                  <div class="error">
+                      {{ $errors->first('email') }}
+                  </div>
+                  @endif
                 </div>
 
                 <div class="col-md-6 ">
-                  <input type="text" class="form-control" name="contact" placeholder="Contact" required="">
+                  <input type="text" class="form-control {{ $errors->has('contact') ? 'error' : '' }}" name="contact" placeholder="Contact" required/>
+                  <!-- Error -->
+                  @if ($errors->has('contact'))
+                  <div class="error">
+                      {{ $errors->first('contact') }}
+                  </div>
+                  @endif
                 </div>
 
                 <div class="col-md-12">
-                  <input type="text" class="form-control" name="subject" placeholder="Sujet" required="">
+                  <input type="text" class="form-control {{ $errors->has('subject') ? 'error' : '' }}" name="subject" placeholder="Sujet" required/>
+                  <!-- Error -->
+                  @if ($errors->has('subject'))
+                  <div class="error">
+                      {{ $errors->first('subject') }}
+                  </div>
+                  @endif
                 </div>
 
                 <div class="col-md-12">
-                  <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+                  <textarea class="form-control {{ $errors->has('message') ? 'error' : '' }}" name="message" rows="6" placeholder="Message" required/></textarea>
+                  <!-- Error -->
+                  @if ($errors->has('message'))
+                  <div class="error">
+                      {{ $errors->first('message') }}
+                  </div>
+                  @endif
                 </div>
 
                 
