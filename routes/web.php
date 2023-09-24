5c0e9df6da2ebb355ac6798e21e8b19ef0abccab
mrseck <ismaelseck11@gmail.com>
2023-09-24 11:12:56 +0000

    formulaire de contact back end en cours..

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\ContactController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
