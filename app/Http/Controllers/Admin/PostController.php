c90b31e690c9f1a9f5983cc2f62b2f370c2c1e11
Ismael Seck <ismaelseck11@gmail.com>
2023-11-16 22:58:01 +0000

    Blog category completed

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\BlogCategory;
 use App\Models\Posts;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
@@ -34,11 +35,47 @@ class PostController extends Controller
         $post->author = $request->input('author');
         $post->description = $request->input('description');
         $post->save();
-        return redirect('/admin/add-post')->with('status', 'poste ajoutée !');
+        return redirect('/admin/add-post')->with('status', 'poste ajouté !');
     }
 
     public function edit($id){
         $post = Posts::find($id);
         return view('admin.blog.posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, $id){
+        $post = Posts::find($id);
+        if($request->hasFile('image'))
+        {
+            $path = 'assets/uploads/posts/'.$post->image;
+            if(File::exists($path))
+            {
+                File::delete($path);
+            }
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $filename = time().'.'.$ext;
+            $file->move('assets\uploads\posts',$filename);
+            $post->image = $filename;
+        }
+        $post->title = $request->input('title');
+        $post->author = $request->input('author');
+        $post->description = $request->input('description');
+        $post->update();
+        return redirect('/admin/post')->with('status', 'poste Modifié !');
+    }
+
+    public function destroy($id){
+        $post = Posts::find($id);
+        if($post->image)
+        {
+            $path = 'assets/uploads/posts/'.$post->image;
+            if(File::exists($path))
+            {
+                File::delete($path);
+            }
+        }
+        $post->delete();
+        return redirect('/admin/post')->with('status', 'Poste Supprimée !');
     }
 }
--- resources/views/admin/blog/posts/edit.blade.php
+++ resources/views/admin/blog/posts/edit.blade.php
@@ -24,23 +24,37 @@
               @csrf
               @method('PUT')
               <div class="row mb-3">
-                <label for="inputText" class="col-sm-2 col-form-label">Nom</label>
+                <label for="inputText" class="col-sm-2 col-form-label">Categories</label>
                 <div class="col-sm-10">
-                  <input type="text" name="name" value="{{ $category->name }}" class="form-control">
+                    <select class="form-select" aria-label="Default select example" name="cate_id">
+                        <option value="">{{ $post->category->name }}</option>
+                    </select>
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label for="inputText" class="col-sm-2 col-form-label">Titre</label>
+                <div class="col-sm-10">
+                  <input type="text" name="title" class="form-control" value="{{ $post->title }}">
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label for="inputText" class="col-sm-2 col-form-label">Auteur</label>
+                <div class="col-sm-10">
+                  <input type="text" name="author" class="form-control" value="{{ $post->author }}">
                 </div>
               </div>
               <div class="row mb-3">
                 <label for="inputText" class="col-sm-2 col-form-label">Description</label>
                 <div class="col-sm-10">
-                    <input type="text" name="description" value="{{ $category->description }}" class="form-control">
+                    <input type="text" name="description" class="form-control" value="{{ $post->description }}">
                 </div>
               </div>
-
+              
               <div class="row mb-3">
                 <label for="inputNumber" class="col-sm-2 col-form-label">File Upload</label>
                 <div class="col-sm-10">
-                    @if($category->image)
-                       <img src="{{ asset('assets/uploads/category/'.$category->image) }}" class="cate-image1" alt="category image">
+                    @if($post->image)
+                       <img src="{{ asset('assets/uploads/posts/'.$post->image) }}" class="cate-image1" alt="category image">
                     @endif
                     <br>
                   <input class="form-control" type="file" id="formFile" name="image">
@@ -48,30 +62,9 @@
               </div>
 
               <div class="row mb-3">
-                <legend class="col-form-label col-sm-2 pt-0"></legend>
-                <div class="col-sm-10">
-
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck1" value="{{ $category->status  == "1" ? 'checked':'' }}" name="status">
-                    <label class="form-check-label" for="gridCheck1">
-                      Status
-                    </label>
-                  </div>
-
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck2" value="{{ $category->popular  == "1" ? 'checked':'' }}" name="popular">
-                    <label class="form-check-label" for="gridCheck2">
-                      Popular
-                    </label>
-                  </div>
-
-                </div>
-              </div>
-
-              <div class="row mb-3">
                 <label class="col-sm-2 col-form-label"></label>
                 <div class="col-sm-10">
-                  <button type="submit" class="btn btn-primary">Submit Form</button>
+                  <button type="submit" class="btn btn-primary">Modifier</button>
                 </div>
               </div>
 
